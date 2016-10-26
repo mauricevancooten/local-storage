8192c2d079b673ca55b095f708c5d473fc7f1f31
Maurice van Cooten <maurice@minus.nz>
2016-10-27 08:58:34 +1300

    added multiple items to cart

--- cart/custom.js
+++ cart/custom.js
@@ -1,53 +1,136 @@
-(function (){ //IIFE
-	
-	function getCartArray() {
-		var cartArray = localStorage['cartArray'];
-		if (!cartArray) {
-			cartArray = [];
-			localStorage.setItem('cartArray', JSON.stringify(cartArray));
-		} else {
-			cartArray = JSON.parse(cartArray);
-		}
-		return cartArray;
-	}
+(function() { //IIFE
 
-	var cartArray = getCartArray();
+    function getCartArray() {
+        var cartArray = localStorage['cartArray'];
+        if (!cartArray) {
+            cartArray = [];
+            localStorage.setItem('cartArray', JSON.stringify(cartArray));
+        } else {
+            cartArray = JSON.parse(cartArray);
+        }
+        return cartArray;
+    }
 
-	// Loop through items in local storage
+    var cartArray = getCartArray();
 
-	for (var i = 0; i < cartArray.length; i++) {
-		var key = cartArray[i];
-		var value = localStorage[key];
-		addCartItems(value)
-	}
+    // Loop through items in local storage
 
-	// Add cart items to DOM
+    for (var i = 0; i < cartArray.length; i++) {
+        var key = cartArray[i];
+        var value = localStorage[key];
+        addCartItems(value)
+    }
 
-	function addCartItems(value) {
-		var cart = document.querySelector('.cart');
-		row = document.createElement('tr');
-		data = document.createElement('td');
-		data.innerHTML = value;
-		row.appendChild(data);
-		cart.appendChild(row);
-	}
+    // console.log(value)
 
-	// Add item to Cart
+    // Get item array
 
-	var button = document.querySelector('.add');
-	button.addEventListener('click', function () {
-		var cartArray = getCartArray();
-		var currentDate = new Date();
-		var key = 'item_' + currentDate.getTime();
-		var itemArray = [];
-		var description = document.querySelector('.description').innerHTML;
-		var price = document.querySelector('.price').innerHTML;
-		itemArray.push({'desc':description})
-		itemArray.push({'price': price})
-		var value = JSON.stringify(itemArray);
-		localStorage.setItem(key, value);
-		cartArray.push(key);
-		localStorage.setItem('cartArray', JSON.stringify(cartArray));
-	});
+    function getItemArray() {
+        if (!itemArray) {
+            itemArray = []
+        }
+        return itemArray;
+    }
+
+    var itemArray = getItemArray();
+
+
+
+    // Add cart items to DOM
+
+    function addCartItems(value) {
+
+        value = JSON.parse(value);
+        // console.log(value)
+        var item;
+        for (item in value) {
+            var desc = value[0].desc
+            var price = value[1].price
+            var quantity = value[2].qty
+        }
+
+        var cart = document.querySelector('.cart');
+        var row = document.createElement('tr');  
+        var dataDesc = document.createElement('td');
+        dataDesc.innerHTML = desc;
+        var dataPrice = document.createElement('td');
+        dataPrice.innerHTML = '$ ' + price;
+        var dataQuantity = document.createElement('td');
+        dataQuantity.innerHTML = quantity;
+        var deleteData = document.createElement('td');
+        var deleteButton = document.createElement('button')
+        deleteButton.setAttribute('class', key);
+				deleteData.appendChild(deleteButton).innerHTML = 'delete';
+				var subTotal = document.createElement('td');
+				subTotal.innerHTML = '$ ' + (price * quantity).toFixed(2);
+        row.appendChild(dataDesc);
+        row.appendChild(dataPrice);
+        row.appendChild(dataQuantity);
+        row.appendChild(subTotal);
+        row.appendChild(deleteData);
+        cart.appendChild(row);
+    }
+
+    // Display total amount of items in the cart
+
+    var itemTotal = cartArray.length;
+    var total = document.querySelector('.item-total');
+    total.innerHTML = itemTotal;
+
+    // Add item to Cart
+
+    var button = document.querySelectorAll('.add');
+    for (var i = 0; i < button.length; i++) {
+
+        button[i].addEventListener('click', function() {
+            var cartArray = getCartArray();
+            var currentDate = new Date();
+            var key = 'item_' + currentDate.getTime();
+            var itemArray = getItemArray();
+            var description = this.parentElement.querySelector('.description').innerHTML;
+            var price = this.parentElement.querySelector('.price').innerHTML;
+            var quantity = this.parentElement.querySelector('.qty').value;
+            itemArray.push({ 'desc': description });
+            itemArray.push({ 'price': price });
+            itemArray.push({ 'qty': quantity });
+            itemArray = JSON.stringify(itemArray)
+            localStorage.setItem(key, itemArray);
+            cartArray.push(key);
+            localStorage.setItem('cartArray', JSON.stringify(cartArray));
+            // Update amount of items in cart
+            itemTotal++;
+            total.innerHTML = itemTotal;
+            location.reload(); // Reload page
+
+        });
+    }
+
+    // var deleteButton = document.querySelector('table').querySelectorAll('button');
+
+    // for (var i = 0; i < deleteButton.length; i++) {
+    // 	deleteButton[i].addEventListener('click', deleteItem)
+    // }
+
+    
+
+    // Delete item
+
+    // function deleteItem(e) {
+    //     var key = e.target.className;
+    //     if (e.target.tagName.toLowerCase() == 'span') {
+    //         key = e.target.parentNode.className;
+    //     }
+    //     localStorage.removeItem(key);
+    //     var cartArray = getCartArray();
+    //     if (cartArray) {
+    //         for (var i = 0; i < cartArray.length; i++) {
+    //             if (key == cartArray[i]) {
+    //                 cartArray.splice(i, 1)
+    //             }
+    //         }
+    //     }
+    //     localStorage.setItem('cartArray', JSON.stringify(cartArray));
+    //     location.reload();
+    // }
 
 })();
